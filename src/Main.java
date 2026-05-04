a502a8f5dad6f14b96a48052bad7f0f258bad936
Maja <majavrbanec94@gmail.com>
2026-05-04 18:54:17 +0200

    " commit"

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.time.LocalDateTime;
 
 public class Main {
+    public static void main(String[] args) {
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+        System.out.println("Dan: " + dt.getDayOfMonth());
+        System.out.println("Sat: " + dt.getHour());
+    }
 }
